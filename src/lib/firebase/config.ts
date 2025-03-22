72384e7e37baf16f52c0ea63e86e0ce56ae98da7
Ryan <163689167+Ryangigs1@users.noreply.github.com>
2025-03-22 17:04:40 +0100

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBr8LZ1rzODvzUy4thQfQY8fCEIhYCZDdM", // Web API Key
   authDomain: "lecture-apps.firebaseapp.com", // Project ID with firebaseapp.com
   projectId: "lecture-apps", // Project ID
-  storageBucket: "lecture-apps.appspot.com", // Corrected storage bucket format
+  storageBucket: "lecture-apps.firebasestorage.app", // Corrected storage bucket format
   messagingSenderId: "852126257336", // Project Number
   appId: "1:852126257336:web:e67fe336ea18d97adb644d", // Replace with actual App ID from Firebase
   measurementId: "G-JMPVWQLKJ7" // Keep this if using Google Analytics
