5e0e03adc159565feeb4534af34d834691b5c471
Ryan <163689167+Ryangigs1@users.noreply.github.com>
2025-03-22 16:28:15 +0100

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,6 +5,6 @@ export const firebaseConfig = {
   projectId: "lecture-apps", // Project ID
   storageBucket: "lecture-apps.appspot.com", // Project ID with appspot.com
   messagingSenderId: "852126257336", // Project Number
-  appId: "1:852126257336:web:your-app-specific-id", // Replace with actual App ID from Firebase
+  appId: "1:852126257336:web:e67fe336ea18d97adb644d", // Replace with actual App ID from Firebase
   measurementId: "G-7ZDFJ7B31K" // Keep this if using Google Analytics
 };
