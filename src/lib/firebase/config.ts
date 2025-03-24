20d484b834d49e6509fb8d6f0dd2c89b5cf1567a
Ryan <163689167+Ryangigs1@users.noreply.github.com>
2025-03-24 22:23:30 +0100

    Add files via upload

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,11 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBr8LZ1rzODvzUy4thQfQY8fCEIhYCZDdM",
-  authDomain: "lecture-apps.firebaseapp.com",
-  projectId: "lecture-apps",
-  storageBucket: "lecture-apps.firebasestorage.app", // Using the correct format for Firebase Storage
-  messagingSenderId: "852126257336",
-  appId: "1:852126257336:web:e67fe336ea18d97adb644d",
-  measurementId: "G-JMPVWQLKJ7"
+  apiKey: "AIzaSyBrq_4i9iL2un0L-aSyj8ylCoJ5E-rBYBk",
+  authDomain: "my-chapel-app.firebaseapp.com",
+  projectId: "my-chapel-app",
+  storageBucket: "my-chapel-app.firebasestorage.app",
+  messagingSenderId: "797146516177",
+  appId: "1:797146516177:web:70983a5317befbf3f253f0",
+  measurementId: "G-W47B9G137P"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
